93af52bd1b7b7d305284d98072833569300e3643
Luiz Picolo <luizpicolo@gmail.com>
2025-04-27 21:25:30 -0400

    Adiciona rota para a lista de usuários no painel administrativo

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/home.vue'
 import Login from '@/views/login.vue'
+import UserList from '@/views/users/list.vue'
 
 const routes = [
   { path: '/', component: Login },
-  { path: '/home', component: Home }
+  { path: '/home', component: Home },
+  { path: '/administrador/usuarios/listar', component: UserList }
 ]
 
 const router = createRouter({
